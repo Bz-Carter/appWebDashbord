473af2ca8e37e6df0711d38f369045a90efbd1b9
Bz-Carter <lebz2016@gmail.com>
2021-07-19 23:57:28 +0000

    paginator endpoint .

--- src/app/secure/components/paginator/paginator.component.ts
+++ src/app/secure/components/paginator/paginator.component.ts
@@ -3,30 +3,28 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
-  styleUrls: ['./paginator.component.css']
+  styleUrls: ['./paginator.component.css'],
 })
 export class PaginatorComponent implements OnInit {
   @Input('lastPage') lastPage;
   @Output('pageChanged') pageChanged = new EventEmitter<number>();
   currentPage = 1;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   prev() {
     if (this.currentPage == 1) return;
-    
+
     this.currentPage--;
     this.pageChanged.emit(this.currentPage);
   }
 
   next() {
     if (this.currentPage == this.lastPage) return;
-    
+
     this.currentPage++;
     this.pageChanged.emit(this.currentPage);
   }
-
 }
