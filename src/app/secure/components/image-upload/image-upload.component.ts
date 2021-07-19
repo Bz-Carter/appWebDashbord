d8865511ac2fc92148d1c31c04559f7677e0e492
Bz-Carter <lebz2016@gmail.com>
2021-07-19 23:59:00 +0000

    Image-upload component .

--- src/app/secure/components/image-upload/image-upload.component.ts
+++ src/app/secure/components/image-upload/image-upload.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-image-upload',
   templateUrl: './image-upload.component.html',
-  styleUrls: ['./image-upload.component.css']
+  styleUrls: ['./image-upload.component.css'],
 })
 export class ImageUploadComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
