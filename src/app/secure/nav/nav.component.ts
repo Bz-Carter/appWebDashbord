20630ba55c817e77adf33c4b1bdc9abddaa1d374
Bz-Carter <lebz2016@gmail.com>
2021-07-20 00:07:09 +0000

    Navigation component created .

--- src/app/secure/nav/nav.component.ts
+++ src/app/secure/nav/nav.component.ts
@@ -4,18 +4,15 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   logout() {
     localStorage.removeItem('token');
     this.router.navigate(['/login']);
   }
- 
 }
